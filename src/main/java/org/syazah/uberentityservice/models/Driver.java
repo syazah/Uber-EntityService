db1b4e49714475859ebcefe01cabf2b523c6004c
Syed Azaan Ahmad <120459599+syazah@users.noreply.github.com>
2025-07-28 10:51:03 +0530

    Added Changes

--- src/main/java/org/syazah/uberentityservice/models/Driver.java
+++ src/main/java/org/syazah/uberentityservice/models/Driver.java
@@ -1,9 +1,9 @@
 package org.syazah.uberentityservice.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
 import lombok.*;
 import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
@@ -16,15 +16,34 @@ import java.util.List;
 @NoArgsConstructor
 @Getter
 @Setter
-@JsonIgnoreProperties({"hibernateLazyInitializer", "handler" , "bookings"})
-public class Driver extends BaseModel{
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler", "bookings"})
+public class Driver extends BaseModel {
 
     private String name;
+
+    private String aadharCard;
 
     @Column(nullable = false, unique = true)
     private String licenseNumber;
 
     private String phoneNumber;
+
+    @Enumerated(value = EnumType.STRING)
+    private DriveApprovalStatus approvalStatus;
+
+    @OneToOne
+    private ExactLocation lastKnownLocation;
+
+    @OneToOne
+    private ExactLocation home;
+
+    @DecimalMin(value = "0.00", message = "Rating must be between 0.00 and 5.00")
+    @DecimalMax(value = "5.00", message= "Rating must be between 0.00 and 5.00")
+    private Double rating;
+
+    private String activeCity;
+
+    private Boolean isAvailable;
 
     // 1 : n , Driver : Booking
     @OneToMany(mappedBy = "driver")
